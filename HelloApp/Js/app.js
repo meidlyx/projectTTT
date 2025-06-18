5455b1590f39a72b4f987d07e0fc9c9e17bb28d8
Dimochka <dima.krokhaliov@yandex.ru>
2025-06-18 22:20:51 +0500

    Мелкие правки--

---
+++ HelloApp/Js/app.js
@@ -0,0 +1,12 @@
+const express = require("express"); 
+const path = require('path')
+const app = express()
+ 
+app.get("/",(req, res) => {
+  
+    res.sendFile(path.join(__dirname,'views','test.html'))
+});
+
+app.listen(3000);
+
+
